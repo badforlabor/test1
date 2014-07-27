e5b592c524d9d79a7094deaa690941285e0092b8
labor.lingyou <labor@lingyou.com>
2014-07-27 18:51:58 +0800

    练习ref、out、directory

---
+++ learn_csharp/api_filesystem/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace api_filesystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 创建递归目录，相对路径目录
+            Directory.CreateDirectory("c:\\1\\2\\3\\");
+            Directory.CreateDirectory("1\\2\\3");
+
+
+            Console.WriteLine("The End");
+            return;
+        }
+    }
+}
---
+++ learn_csharp/keyword_ref_out/Class1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace keyword_ref_out
+{
+    public class Class1
+    {
+        int a = 0;
+        Class1 func(Class1 c1)
+        {
+            c1 = new Class1();
+            c1.a = 2;
+            return c1;
+        }
+        Class1 fun1(ref Class1 c1)
+        {
+            //c1 = new Class1();
+            c1.a = 2;
+            return c1;
+        }
+        Class1 func2(out Class1 c1)
+        {
+            c1 = new Class1();
+            c1.a = 2;
+            return c1;
+        }
+        void Show()
+        {
+            Console.WriteLine("------------- a=" + a);
+        }
+
+        static void ASSERT(bool exp)
+        {
+            if (exp)
+            {
+                Console.WriteLine("----------- assert true!");
+            }
+            else
+            {
+                Console.WriteLine("----------- assert false!");
+            }
+        }
+
+        static void Main()
+        { 
+            Class1 c1 = new Class1();
+
+            c1.a = 1;
+            ASSERT(c1 == c1.func(c1));
+            c1.Show();
+
+            c1.a = 1;
+            ASSERT(c1 == c1.fun1(ref c1));
+            c1.Show();
+
+            c1.a = 1;
+            ASSERT(c1 == c1.func2(out c1));
+            c1.Show();
+        }
+
+
+    }
+}
