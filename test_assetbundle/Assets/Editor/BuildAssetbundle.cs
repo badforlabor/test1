8349812507d98fbb8e89b4fc2880e1e9613b78de
labor <labor@gmail.com>
2014-08-02 00:38:05 +0800

    测试assetbundle

---
+++ test_assetbundle/Assets/Editor/BuildAssetbundle.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using System.IO;
+
+public class BuildAssetbundle : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    [MenuItem("AssetBundle/打包")]
+    static void BuildAssetBundles()
+    {
+        string Output = Application.dataPath + "/../AssetBundle/a";
+        
+        Directory.CreateDirectory(Output);
+
+        BuildPipeline.PushAssetDependencies();
+        UnityEngine.Object obj = AssetDatabase.LoadAssetAtPath("Assets/AuroraRing.tga", typeof(UnityEngine.Object));
+        BuildPipeline.BuildAssetBundle(obj, null, Output + "/2.assetbundle");
+
+        BuildPipeline.PushAssetDependencies();
+        obj = AssetDatabase.LoadAssetAtPath("Assets/Materials/AuroraRing.mat", typeof(UnityEngine.Object));
+        BuildPipeline.BuildAssetBundle(obj, null, Output + "/3.assetbundle");
+        BuildPipeline.PopAssetDependencies();
+
+        BuildPipeline.PopAssetDependencies();
+    }
+
+}
---
+++ test_assetbundle/Assets/TestAssetBundle.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestAssetBundle : MonoBehaviour {
+
+	// Use this for initialization
+	IEnumerator Start () {
+        UnityEngine.Object templ = Resources.Load("ring");
+        GameObject go = UnityEngine.Object.Instantiate(templ) as GameObject;
+
+        System.Diagnostics.Debug.Assert(go != null);
+
+        go.transform.localRotation = new Quaternion(0,-90,0,0);
+
+        yield return null;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
