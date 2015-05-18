8124347ca16e401f7a4dcf7daa961c9a6824a88f
labor.lingyou <labor@lingyou.com>
2015-05-18 11:16:13 +0800

    朝向摄像机

---
+++ test_unity_api/Assets/autofacetocamera.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class autofacetocamera : MonoBehaviour {
+
+    public Camera _camera = null;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (_camera == null)
+        {
+            Camera[] cameras = GameObject.FindObjectsOfType<Camera>();
+            int layermask = 1 << gameObject.layer;
+            foreach (var c in cameras)
+            {
+                if (c != null && c.enabled && ((c.cullingMask & layermask) > 0))
+                {
+                    _camera = c;
+                    break;
+                }
+            }
+        }
+
+        if (_camera != null)
+        {
+            transform.rotation = _camera.transform.rotation;
+        }
+	}
+}
---
+++ test_unity_api/Assets/testq1.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class testq1 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	    Quaternion basePos = transform.rotation;
+        Quaternion basePos2 = Quaternion.AngleAxis(30, Vector3.zero);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
